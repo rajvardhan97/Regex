a9ab68fca1982974f57c6386810d60987438e8fb
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-28 12:15:28 +0530

    [Raj]Add: Regex to enter valid First Name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Registration registration = new Registration();
+            Console.WriteLine("Enter the First Name");
+            string fName = Console.ReadLine();
+            registration.checkfirstname(fName);
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/Registration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class Registration
+    {
+        public string FirstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        
+        
+        public void checkfirstname(string Firstname)
+        {
+            Regex Regex = new Regex(FirstName);
+            if(Regex.IsMatch(Firstname))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is Invalid");
+            }
+        }
+       
+    }
+}
